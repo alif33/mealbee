f0416e63b3a3dd2f614e4391ccec2d1f58aa9227
ismail alif <43023388+alif33@users.noreply.github.com>
2024-02-08 22:23:03 +0600

    padding added

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,10 +4,12 @@ export default function Blog(){
     return(
         <>
             <div className="main">
-                <h1 className="text-lg font-bold">Home</h1>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe voluptatibus aliquam maiores vero ducimus atque tempore earum consectetur quis veniam?</p>
-                <div className="flex justify-center">
-                    <Link to="/blog">blog</Link>
+                <div className="pt-5">
+                    <h1 className="text-lg font-bold">Home</h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe voluptatibus aliquam maiores vero ducimus atque tempore earum consectetur quis veniam?</p>
+                    <div className="flex justify-center">
+                        <Link to="/blog">blog</Link>
+                    </div>
                 </div>
             </div>
             <div className="h-10">
