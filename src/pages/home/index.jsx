8cefed31967ae24bc920be77db97b8ba39245913
ismail alif <43023388+alif33@users.noreply.github.com>
2024-02-08 22:28:43 +0600

    13

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,7 +4,7 @@ export default function Blog(){
     return(
         <>
             <div className="main">
-                <div className="pt-10">
+                <div className="pt-13">
                     <h1 className="text-lg font-bold">Home</h1>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe voluptatibus aliquam maiores vero ducimus atque tempore earum consectetur quis veniam?</p>
                     <div className="flex justify-center">
