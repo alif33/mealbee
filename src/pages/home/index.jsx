3c2cb17f42d63c5fb6771ae664fd3e5b1a4dd90a
ismail alif <43023388+alif33@users.noreply.github.com>
2024-02-08 22:26:14 +0600

    maybe okay

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,7 +4,7 @@ export default function Blog(){
     return(
         <>
             <div className="main">
-                <div className="pt-14">
+                <div className="pt-10">
                     <h1 className="text-lg font-bold">Home</h1>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe voluptatibus aliquam maiores vero ducimus atque tempore earum consectetur quis veniam?</p>
                     <div className="flex justify-center">
