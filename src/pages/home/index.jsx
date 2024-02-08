4747ef4d624698ab3e1e18750ec50e8d104b7595
ismail alif <43023388+alif33@users.noreply.github.com>
2024-02-08 22:33:46 +0600

    padding remove

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,13 +4,13 @@ export default function Blog(){
     return(
         <>
             <div className="main">
-                <div className="pt-13">
+                {/* <div className="pt-13"> */}
                     <h1 className="text-lg font-bold">Home</h1>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe voluptatibus aliquam maiores vero ducimus atque tempore earum consectetur quis veniam?</p>
                     <div className="flex justify-center">
                         <Link to="/blog">blog</Link>
                     </div>
-                </div>
+                {/* </div> */}
             </div>
             <div className="h-10">
                 <ul className="flex justify-between font-semibold">
