ddb1f04d12f7ddcb72df65c4ba97e9fb81563457
ismail alif <43023388+alif33@users.noreply.github.com>
2024-02-08 22:04:56 +0600

    padding added

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import Blog from "./pages/blog"
 function App() {
   return (
     <div className="max-w-xl min-h-screen mx-auto bg-black text-white">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/blog" element={<Blog/>} />
-        </Routes>
-      </BrowserRouter>
+      <div className="pt-2 px-3">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/blog" element={<Blog/>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
